339bc0a8b1dbbacf014e1390365f17665b9f5d42
Jbang Macbook <jrbangit03@Jbangs-MacBook-Air.local>
2025-02-04 00:39:21 +0900

    add notes

--- crowd_checker/crowd_checker_api/models.py
+++ crowd_checker/crowd_checker_api/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 
 
+# Encapsulating all model related operations here.
+
+# A class that inherits the Model.
+# Each instance of this class will contain data that represents a row,
+# this allows us to manipulate the data in object-oriented manner.
 class Establishments(models.Model):
     country = models.CharField(max_length=100)
     city = models.CharField(max_length=50)
--- crowd_checker/crowd_checker_api/serializers.py
+++ crowd_checker/crowd_checker_api/serializers.py
@@ -2,10 +2,16 @@ from rest_framework import serializers
 
 from .models import Establishments
 
-
-### TO-DO: I want to use gRPC
+### TO-DO: I want to use gRPC (is it possible in django?)
 
 # This is to serialize the model into json including all fields of the model.
+
+# This file will contain all of SerDe related operations (1 serializer per model)
+# which promotes separation of concern that allows
+# the application to be easily maintained and understand.
+
+# The usage of ModelSerializer allows us to create different types of serializer depending on use case.
+# Which exhibits polymorphism.
 class EstablishmentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Establishments
